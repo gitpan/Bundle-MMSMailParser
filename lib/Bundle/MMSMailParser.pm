93f4463d036202abca792c0dfdd777a77f40ecd5
Rob Lee <robl@cpan.org>
2005-09-05 11:42:15 +0000

    Import of ROBL/Bundle-MMSMailParser-0.01 from CPAN.
    gitpan-cpan-distribution: Bundle-MMSMailParser
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         ROBL/Bundle-MMSMailParser-0.01.tar.gz
    gitpan-cpan-author:       ROBL
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/MMSMailParser.pm
@@ -0,0 +1,52 @@
+package Bundle::MMSMailParser;
+
+use warnings;
+use strict;
+
+our $VERSION = '0.01';
+
+1; # End of Bundle::MMSMailParser
+
+__END__
+
+=head1 NAME
+
+Bundle::MMSMailParser - Bundle of MMS parsing modules
+
+=head1 SYNOPSIS
+
+C<perl -MCPAN -e 'install Bundle::MMSMailParser'>
+
+=head1 CONTENTS
+
+MMS::MailMessage
+MMS::MailMessage::ProviderParsed
+MMS::ProviderMailParser
+MMS::ProviderMailParser::UKVodafone
+MMS::MailParser
+
+=head1 DESCRIPTION
+
+The complete set of modules required to parse MMS mail messages.
+
+=head1 AUTHOR
+
+Rob Lee robl@robl.co.uk
+
+=head1 LICENSE
+
+Copyright 2005 Rob Lee, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+To quote the perl artistic license ('perldoc perlartistic') :
+
+10. THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+    WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES
+    OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+=cut
+
+
+
